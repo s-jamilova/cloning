907f373abf68e804cf248938de857a9943c27d27
attilacoder <32287233+attilacoder@users.noreply.github.com>
2021-03-01 08:38:48 -0800

    fixing bugs

--- TextExcel/src/textExcel/EmptyCell.java
+++ TextExcel/src/textExcel/EmptyCell.java
@@ -10,5 +10,8 @@ public class EmptyCell implements Cell
     {
         return "";
     }
-    public double getDoubleValue();
+    public double getDoubleValue()
+    {
+        return 0;
+    }
 }
