1390b31c5284a776ab4e6ac3caba686854f44d1b
attilacoder <32287233+attilacoder@users.noreply.github.com>
2021-03-01 16:58:03 -0800

    wrapping up checkpoint 1

--- TextExcel/src/textExcel/SpreadsheetLocation.java
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -27,7 +27,7 @@ public class SpreadsheetLocation implements Location
         String strCol;
         String strRow;
         strCol = cellName.substring(0,1);
-        strRow = cellName.substring(1,2);
+        strRow = cellName.substring(1);
         col = getColumnNumberFromColumnLetter(strCol);
         row = Integer.parseInt(strRow);
     }
