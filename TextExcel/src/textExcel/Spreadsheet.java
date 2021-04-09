620c4114ad57b0c0efdc43b742b14a95dca4c78d
attilacoder <32287233+attilacoder@users.noreply.github.com>
2021-04-09 01:39:12 -0700

    c2

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -50,7 +50,8 @@ public class Spreadsheet implements Grid
 		}
 		else if (command.contains("\"")) {
 			String stringValue = command.substring(command.indexOf("=") + 2);
-			stringValue = stringValue.replaceAll("\"", "");
+			stringValue = stringValue.substring(1);
+			stringValue = stringValue.substring(-1);
 			Location loc = new SpreadsheetLocation(command.substring(0,2));
 			c[loc.getRow()][loc.getCol()] = new TextCell(stringValue);
 			return getGridText();
